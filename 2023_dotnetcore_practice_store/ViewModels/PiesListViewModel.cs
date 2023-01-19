245f1fc4c22b897c0ef527e9537fb840631b52f8
Sarah Gilbreath <thenoiseinspace@gmail.com>
2023-01-19 13:59:44 -0500

    updating IEnumerables

---
+++ 2023_dotnetcore_practice_store/ViewModels/PiesListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+
+namespace _2023_dotnetcore_practice_store.ViewModels
+{
+    public class PiesListViewModel
+    {
+        public IEnumerable <Pie> Pies { get; set; }
+        //he creates a Using Statement for pie above, but doesn't say how he does it. He selects using BethanysPieShop.Models;
+        //hit alt enter for something close
+        public string CurrentCategory { get; set; }
+    }
+}
